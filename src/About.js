33211f2eb947c655a8fc2113955408d8a6a5e866
Emily Marie Garrett <e.marie.garrett@gmail.com>
2018-03-09 15:16:25 -0800

    weather

--- src/About.js
+++ src/About.js
@@ -1,12 +1,90 @@
-import React from 'react';
+import React, {Component} from 'react';
+const axios = require('axios');
 
-const About = () => (
-  <div>
-    <h1>About me</h1>
-    <p>
-      I'm a big gay 
-    </p>
-  </div>
-)
+class About extends Component {
+  
+	constructor(props) {
+		super(props) 
+		this.state = {
+			zip: 0,
+			weather: null
+		}
+		this.handleChange = this.handleChange.bind(this);
+   		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+	    this.setState({zip: event.target.value})
+	    console.log('Your zip code is' + this.state.zip)
+	}
+
+	kelvinToFahrenheit = (temp) => {
+		var newTemp = (9/5)*(temp - 273) + 32
+		return Math.floor(newTemp)
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+		var base = this
+  		//url for api
+  		let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?zip='+ this.state.zip +',us&appid=052f26926ae9784c2d677ca7bc5dec98'
+
+  		axios({
+  			method: 'GET',
+  			url: weatherApi
+  		})
+  			.then((response) => {
+  				base.setState({
+  					weather: response.data
+  				})
+  			})
+	}
+
+	render() {
+
+		if( this.state.weather !== null ) {
+			var temperature = this.kelvinToFahrenheit(this.state.weather.main.temp) + '°F'
+			var humidity = 'Humidity: '+ this.state.weather.main.humidity + '%'
+			var windspeed = 'Wind: ' + this.state.weather.wind.speed + 'mph'
+			var weatherDesc = this.state.weather.weather[0].main
+			var place = 'Here is the weather in ' + this.state.weather.name + ' today!'
+		} else {
+			var temperature = ' '
+			var humidity = ' '
+			var windspeed = ' '
+			var weatherDesc = ' '
+			var place = ' '
+		}
+
+		return(
+			<div>
+			    <div>
+       
+		      	<form onSubmit={this.handleSubmit}>
+		      	<h2> {place} </h2>
+		        <p className='temp'>
+		      		{temperature}
+		        </p>
+		        <p className='weather'>
+		      		{humidity}<br/>
+		      		{windspeed}<br/>
+		      		{weatherDesc}
+		        </p>
+		        <label>
+		          Please enter your zip code for the weather:
+		          <input type="text" onChange={this.handleChange} />
+		        </label>
+		        <input type="submit" value="Get my forecast!" />
+		      </form>
+		      </div>
+			</div>
+  		)
+
+	}
+
+
+
+  
+}
 
 export default About;
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
+const axios = require('axios');
 
-const Home = () => (
-  <div>
-    <h1>Welcome to the Lemon Bloggo</h1>
-    <img src='http://pngimg.com/uploads/lemon/lemon_PNG25203.png' />
-  </div>
-)
+class Home extends Component {
+  	
+  	constructor(props) {
+  		super(props)
+  		this.state = {
+  			shakespeare: ''
+  		}
+  	}
+
+  	componentDidMount() {
+  		//api call
+  		//save a refernece to 'this' because the value of 'this' will change
+  		//inside the different callback functions
+
+  		var base = this
+  		//url for api
+  		let poemApi = 'http://www.shakeitspeare.com/api/poem'
+  		// our fetch of that poemApi
+  		// fetch(poemApi)
+  		// 	.then((response) => {
+  		// 		//returns as json
+  		// 		return response.json()
+  		// 	})
+  		// 	.then((json) => {
+  		// 		base.setState({
+  		// 			shakespeare: json.poem
+  		// 		})
+  		// 	})
+  		// 	.catch((error) => {
+  		// 		console.log('an error occurred whilst parsing: ', error)
+  		// 	})
+
+  		// axios.get(poemApi)
+  		// 	.then((response) => {
+  		// 		base.setState({
+  		// 			shakespeare: response.data.poem
+  		// 		})
+  		// 	})
+  		// 	.catch((error) => {
+  		// 		console.log('an error occurred whilst parsing: ', error)
+  		// 	})
+
+  		axios({
+  			method: 'GET',
+  			url: poemApi
+  		})
+  			.then((response) => {
+  				base.setState({
+  					shakespeare: response.data.poem
+  				})
+  			})
+  	}
+
+	render() {
+
+		if (this.state.shakespeare) {
+			var poetry = this.state.shakespeare
+		} else {
+			var poetry = "Loading..."
+		}
+
+		return(
+		<div>
+			<h1>Welcome to the Lemon Bloggo</h1>
+			<img src='http://pngimg.com/uploads/lemon/lemon_PNG25203.png' />
+
+			<h2>Fuck: </h2>
+			<p> {poetry} </p>
+		</div>
+  		)
+	}
+}
 
 export default Home;
